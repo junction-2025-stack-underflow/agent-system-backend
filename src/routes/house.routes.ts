151c53558fe4bc09807887aa45a684cb68997c9e
Wassim <512.amosimo@gmail.com>
2025-07-19 09:07:44 +0100

    feat: Implemented the bulk house recommendation route

--- src/routes/house.routes.ts
+++ src/routes/house.routes.ts
@@ -7,12 +7,13 @@ import {
 } from '../controllers/house.controller';
 import { upload } from '../middlewares/image.upload';
 import { authenticateAgency } from '../middlewares/auth.middleware';
-import { recommendClientsForHouse } from '../controllers/recommendation.controller';
+import { recommendClientsForAllHouses, recommendClientsForHouse } from '../controllers/recommendation.controller';
 const router = express.Router();
 router.post('/', upload.array('images', 10), addHouse);
 router.delete('/:id', deleteHouse);
 router.get('/myhouses', getHousesByAgency);
 router.get('/myhouse/:id', getHouseById);
+router.get("/recommendations", recommendClientsForAllHouses)
 router.get('/:id/recommendations', recommendClientsForHouse);
 
 export default router;
