5d8a70bfb628edec0f03187ce47e241e4459dbc1
Wassim <512.amosimo@gmail.com>
2025-07-19 01:37:22 +0100

    comm with AI

--- src/controllers/recommendation.controller.ts
+++ src/controllers/recommendation.controller.ts
@@ -1,6 +1,16 @@
 import { Response } from "express";
 import { AuthRequest } from "../types/express";
+import House from "../models/House";
+import axios from "axios";
 
 export const recommendClientsForHouse = async (req: AuthRequest, res: Response) => {
-  res.status(200).json({ message: "hello world" });
+  try {
+    const house = await House.find({ "details.ID": req.params.id })
+    const response = await axios.get(`${process.env.AI_SERVICE}/match/${req.params.id}`)
+    console.log(response.data);
+    res.status(200).json({ success: true, data: response.data });
+
+  } catch (error:any) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
